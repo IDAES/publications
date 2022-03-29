f6d3f40ce2ff35d4e46b594d441fdde4e382e1dc
Robert Parker <robbybparker@gmail.com>
2022-03-29 13:47:27 -0400

    setup.py and requirements.txt

---
+++ parker_dycops2022/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="parker_dycops2022",
+    description=(
+        'Code to produce the results of "Dynamic Modeling and Nonlinear '
+        'Model Predictive Control of a Moving Bed Chemical Looping '
+        'Reactor", Robert Parker and Lorenz Biegler. Presented at DYCOPS '
+        '2022.'
+    ),
+    author="Robert Parker",
+    author_email="rparker1@andrew.cmu.edu",
+    license="BSD-3",
+    packages=["parker_dycops2022"],
+)
