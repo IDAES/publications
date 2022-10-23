112b1f9e7447e7fd0fd5bc4f7a70642367ecfb73
robbybp <robbybparker@gmail.com>
2022-10-23 15:21:12 -0600

    rename directory; add dae_utils

---
+++ parker_focapo2023/parker_focapo2023/clc/idaes_1_12_gas_solid_contactors/properties/methane_iron_OC_reduction/tests/test_CLC_gas_prop.py
@@ -0,0 +1,101 @@
+#################################################################################
+# The Institute for the Design of Advanced Energy Systems Integrated Platform
+# Framework (IDAES IP) was produced under the DOE Institute for the
+# Design of Advanced Energy Systems (IDAES), and is copyright (c) 2018-2021
+# by the software owners: The Regents of the University of California, through
+# Lawrence Berkeley National Laboratory,  National Technology & Engineering
+# Solutions of Sandia, LLC, Carnegie Mellon University, West Virginia University
+# Research Corporation, et al.  All rights reserved.
+#
+# Please see the files COPYRIGHT.md and LICENSE.md for full copyright and
+# license information.
+#################################################################################
+"""
+Tests for CLC gas phase thermo state block; tests for construction and solve
+Author: Chinedu Okoli
+"""
+
+import pytest
+
+from pyomo.environ import (ConcreteModel,
+                           TerminationCondition,
+                           SolverStatus,
+                           Var)
+
+from idaes.core import FlowsheetBlock
+
+from idaes.core.util.model_statistics import degrees_of_freedom
+
+from idaes.core.util.testing import initialization_tester
+from idaes.core.util import get_solver
+
+from parker_focapo2023.clc.idaes_1_12_gas_solid_contactors.properties.methane_iron_OC_reduction. \
+    gas_phase_thermo import GasPhaseParameterBlock
+
+# Get default solver for testing
+solver = get_solver()
+
+
+# -----------------------------------------------------------------------------
+@pytest.fixture(scope="class")
+def gas_prop():
+    m = ConcreteModel()
+    m.fs = FlowsheetBlock(default={"dynamic": False})
+
+    # gas properties and state inlet block
+    m.fs.properties = GasPhaseParameterBlock()
+    m.fs.unit = m.fs.properties.build_state_block(
+        default={"parameters": m.fs.properties,
+                 "defined_state": True})
+
+    m.fs.unit.flow_mol.fix(1)
+    m.fs.unit.temperature.fix(450)
+    m.fs.unit.pressure.fix(1.60)
+    m.fs.unit.mole_frac_comp["CO2"].fix(0.4772)
+    m.fs.unit.mole_frac_comp["H2O"].fix(0.0646)
+    m.fs.unit.mole_frac_comp["CH4"].fix(0.4582)
+
+    return m
+
+
+def test_build_inlet_state_block(gas_prop):
+    assert isinstance(gas_prop.fs.unit.mw, Var)
+    assert isinstance(gas_prop.fs.unit.dens_mol, Var)
+    assert isinstance(gas_prop.fs.unit.dens_mol_comp, Var)
+    assert isinstance(gas_prop.fs.unit.dens_mass, Var)
+    assert isinstance(gas_prop.fs.unit.cp_mol_comp, Var)
+    assert isinstance(gas_prop.fs.unit.cp_mol, Var)
+    assert isinstance(gas_prop.fs.unit.cp_mass, Var)
+    assert isinstance(gas_prop.fs.unit.visc_d, Var)
+
+
+def test_setInputs_state_block(gas_prop):
+    assert degrees_of_freedom(gas_prop.fs.unit) == 0
+
+
+def test_initialize(gas_prop):
+    initialization_tester(
+            gas_prop)
+
+
+def test_solve(gas_prop):
+
+    assert hasattr(gas_prop.fs.unit, "mw")
+    assert hasattr(gas_prop.fs.unit, "cp_mol")
+    assert hasattr(gas_prop.fs.unit, "enth_mol")
+
+    results = solver.solve(gas_prop)
+
+    # Check for optimal solution
+    assert results.solver.termination_condition == \
+        TerminationCondition.optimal
+    assert results.solver.status == SolverStatus.ok
+
+
+def test_solution(gas_prop):
+    assert (pytest.approx(1, abs=1e-2) ==
+            gas_prop.fs.unit.mw.value)
+    assert (pytest.approx(1, abs=1e-2) ==
+            gas_prop.fs.unit.cp_mol.value)
+    assert (pytest.approx(1, abs=1e-2) ==
+            gas_prop.fs.unit.enth_mol.value)
--- parker_focapo2023/parker_focapo2023/idaes_1_7_gas_solid_contactors/properties/methane_iron_OC_reduction/__init__.py
+++
@@ -1,9 +0,0 @@
-from parker_focapo2023.idaes_1_7_gas_solid_contactors.properties.methane_iron_OC_reduction. \
-      gas_phase_thermo import (GasPhaseThermoParameterBlock,
-                               GasPhaseThermoStateBlock)
-from parker_focapo2023.idaes_1_7_gas_solid_contactors.properties.methane_iron_OC_reduction. \
-    solid_phase_thermo import (SolidPhaseThermoParameterBlock,
-                               SolidPhaseThermoStateBlock)
-from parker_focapo2023.idaes_1_7_gas_solid_contactors.properties.methane_iron_OC_reduction. \
-    hetero_reactions import (HeteroReactionParameterBlock,
-                             ReactionBlock)
